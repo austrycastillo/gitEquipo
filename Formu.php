7013f85066c33e5b0b3031aae50351139059918b
alexis <alex10o@hotmail.com>
2021-07-09 04:15:08 -0300

    agregue un php y modifique el html

---
+++ Formu.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Formulario</title>
+  </head>
+  <body BGCOLOR="blue">
+  Formulario <br /> <br /> <br />
+  Tipo Documento: <input type="text" value="" name="tipo" /> &#160; &#160; &#160;
+  Documento: <input type="text" value="" name="docu" /> <br   /> <br />
+  Apellido: <input type="text" value="" name="nom" /> &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160;
+  Nombre: <input type="text" value="" name="apell" /> <br   /> <br />
+  Domicilio: <input type="text" value="" name="dom" /> &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160;
+  Domicilio de Trabajo: <input type="text" value="" name="domt" /> <br   /> <br />
+  Sexo: <select name="sexo">
+        <option value="1"> Masculino </option>
+        <option value="2"> Femenino </option>
+        </select> &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160; 
+  Feche Nacimiento: <input type="text" value="" name="fecha" /> <br   /> <br />
+  Telefono Fijo: <input type="text" value="" name="tel" /> &#160; &#160; &#160; &#160; &#160;
+  Telefono Movil: <input type="text" value="" name="telm" /> &#160; &#160; &#160;
+  Telefono Trabajo: <input type="text" value="" name="telt" /> <br   /> <br />
+  Localidad: <input type="text" value="" name="local" /> &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160;
+  Provincia: <input type="text" value="" name="prov" /> &#160; &#160; &#160; &#160; &#160; &#160; &#160; &#160;
+  Pais: <input type="text" value="" name="pais" /> <br   /> <br />
+  Nacionalidad: <input type="text" value="" name="nacio" /> &#160; &#160; &#160; &#160; &#160; &#160;
+  E-mail: <input type="text" value="" name="correo" /> <br   /> <br />
+
+  </body>
+</html>
